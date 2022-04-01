fdd5017e0700d47e436b962233ab4df17f38852e
Nickolas de Luca Alberton <nickolasdeluca@live.com>
2022-04-01 19:01:37 -0300

    Projeto de teste da primeira aula

---
+++ aulaum/App.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{textAlign: "center", color: "red", fontSize: 24, marginTop: 200}}>
+        Hello world!
+      </Text>
+    </View>
+  );
+}
+
+export default App;
